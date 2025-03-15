f1748abcaac55dd4225f1a683237f4fa3560dbe9
Your Name <chizupraise31@gmail.com>
2025-03-15 13:05:41 +0100

    test

--- client/src/services/apiService.tsx
+++ client/src/services/apiService.tsx
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// const API_BASE_URL = "http://localhost:5000/api";
-// const API_BASE_URL = "https://xyranetserver.onrender.com/api";
-
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Upload file function with parameter typing
